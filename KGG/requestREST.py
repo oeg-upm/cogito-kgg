5e65e82a123e2e5b02e019ca53e57cabb0664348
Salva597 <salgonger1997@gmail.com>
2021-10-22 10:02:47 +0200

    Fixed problem with JSESSIONID

--- KGG/requestREST.py
+++ KGG/requestREST.py
@@ -15,16 +15,18 @@ def materialise(file):
 
     payload = {}
 
+    s = requests.Session()
+    s.get('https://kgg.openmetrics.eu/')
+
     headers_post = {
-    'Cookie': 'JSESSIONID=2035B43B86BE40748581C7BECDEE87F1'
+    'Cookie': 'JSESSIONID=' + s.cookies.get_dict()['JSESSIONID']
     }
 
     headers_get = {
-    'Cookie': 'JSESSIONID=2035B43B86BE40748581C7BECDEE87F1'
+    'Cookie': 'JSESSIONID=' + s.cookies.get_dict()['JSESSIONID']
     }
 
     response_post = requests.request("POST", url_post, headers=headers_post, data=payload, files=files)
     response_get = requests.request("GET", url_get, headers=headers_get, data=payload)
-    
 
     return response_get.text
--- app.py
+++ app.py
@@ -10,12 +10,14 @@ def post_file():
     if request.method == 'POST':
         uploaded_file = request.files['file'].read()
         temp = tempfile.NamedTemporaryFile() # mode='w+t'
+        temp2 = tempfile.NamedTemporaryFile
         try:
             temp.write(uploaded_file)
             temp.seek(0)
             response = materialise(temp.name)
-            temp.flush()
+            temp.truncate()
             temp.write(bytes(response, 'utf-8'))
+            temp.seek(0)
             return send_file(temp.name, as_attachment=True)
         finally:
             temp.close()
